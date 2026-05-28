d2e0909e92a7dbae3cb5296fc774699ac294e5b8
santiagorenteriarivas-droid <santiagorenteriarivas@gmail.com>
2026-05-28 18:17:18 -0500

    Delete FrontPhp_AppiGenericaPhp directory

--- FrontPhp_AppiGenericaPhp/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// URL base de la API genérica de Innovación Curricular
-define('API_BASE_URL', 'http://localhost:8000');
-?>
-
--- FrontPhp_AppiGenericaPhp/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Redirige siempre al dashboard principal
-header('Location: pages/home.php'); 
-exit;
-?>
-
